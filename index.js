536e923a88e8896af2e369c92b16909c3ead5e45
Silis Kleemoff <kleemoffdeveloper@gmail.com>
2023-04-24 11:45:11 -0400

    uri encoding

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ form.addEventListener('submit', (event) => {
 
     emailDestination = mailtoField.value ? mailtoField.value : emailDestination
 
-    body = `Product Title: ${productTitle.value}, Short Description: ${shortDescription.value}, Product Description: ${productDescription.value}`
-
+    body = `Product Title: ${productTitle.value}\n\n Short Description: ${shortDescription.value}\n\n Product Description: ${productDescription.value}`
+    body = encodeURIComponent(body)
     window.open(`mailto:${emailDestination}?subject=${subject}&body=${body}`)
 })
 
